3f83e4d094bab4d6a610dbe772b61d4146eea293
Aa Freelance <aa_freelance@gmail.com>
2017-11-22 00:46:23 +0530

    Config setup + gitignore + README

--- config.py
+++ config.py
@@ -1,5 +1,22 @@
 import os
-SECRET_KEY="MY_SECRET_KEY"
-SQLALCHEMY_DATABASE_URI= os.environ['DATABASE_URL']
-# export DATABASE_URL="postgres://postgres:root@localhost:5432/blog_app
-SQLALCHEMY_TRACK_MODIFICATIONS=False
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config():
+    """Common config"""
+    SECRET_KEY="MY_SECRET_KEY"
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
+
+class DevConfig(Config):
+    DEBUG=True
+    # SQLALCHEMY_DATABASE_URI= os.environ['DATABASE_URL']
+    SQLALCHEMY_DATABASE_URI="postgres://postgres:root@localhost:5432/blog_app"
+    # export DATABASE_URL="postgres://postgres:root@localhost:5432/blog_app"
+
+class ProdConfig(Config):
+    DEBUG=False
+
+class TestConfig(Config):
+    TESTING=True
+    WTF_CSRF_ENABLED=False
+    SQLALCHEMY_DATABASE_URI="postgres://postgres:root@localhost:5432/blog_app"
